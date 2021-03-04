5a41f57e9ae1c89b11492ce9f42f456a84ea5aff
Illuminati-CRAZ <42056854+Illuminati-CRAZ@users.noreply.github.com>
2021-03-03 17:44:36 -0800

    Add files via upload

--- plugin.lua
+++ plugin.lua
@@ -21,9 +21,9 @@ function draw()
     local oneSided = state.GetValue("oneSided")
     if oneSided == nil then oneSided = false end
 
-    if imgui.Button("Current") then start = state.SongTime end imgui.SameLine()
+    if imgui.Button("Current") then start = state.SelectedHitObjects[1] and state.SelectedHitObjects[1].StartTime or state.SongTime end imgui.SameLine()
     _, start = imgui.InputFloat("Start", start, 1)
-    if imgui.Button("Current##1") then stop = state.SongTime end imgui.SameLine()
+    if imgui.Button("Current##1") then stop = state.SelectedHitObjects[1] and state.SelectedHitObjects[1].StartTime or state.SongTime end imgui.SameLine()
     _, stop = imgui.InputFloat("Stop", stop, 1)
     _, amplitude = imgui.InputFloat("Start Amplitude", amplitude, 1)
     _, stopamplitude = imgui.InputFloat("Stop Amplitude", stopamplitude, 1)
@@ -54,33 +54,35 @@ function draw()
             for _, time in pairs(noteTimes) do
                 table.insert(notePositions, getPositionFromTime(time))
             end
+            
+            debug = notePositions[1] or "error"
         end
         
         --vibe
-        if useSnap then
-            vibe(start, stop, amplitude, 60000 / map.GetTimingPointAt(state.SongTime).Bpm / increment, stopamplitude, tp_increment, oneSided)
-        else
-            vibe(start, stop, amplitude, increment, stopamplitude, tp_increment, oneSided)
-        end
+        increment = useSnap and 60000 / map.GetTimingPointAt(state.SongTime).Bpm / increment or increment
+        vibe(start, stop, amplitude, increment, stopamplitude, tp_increment, oneSided)
         
         --restore positions
         if preserveNotePositions then
+            local old_sv_count = #map.ScrollVelocities
             
             performQueue()
             resetQueue()
             resetCache()
         
-            --create a list of sorted svs, since when there are many svs,
-            --there is a chance the game doesn't sort the svs in time
+            --with enough SVs, the game is unable to sort added SVs in time
+            --local sorted_svs = correctSVIndexsAtEnd(map.ScrollVelocities, old_sv_count + 1)
             local sorted_svs = table.sort(map.ScrollVelocities, function(a, b) return a.StartTime < b.StartTime end)
-        
+            
             local newPositions = {}
             for _, time in pairs(noteTimes) do
                 table.insert(newPositions, getPositionFromTime(time, sorted_svs))
             end
             
+            debug = debug .. ", " .. newPositions[1] or "error"
+            
             for i = 1, #noteTimes do
-                displace(noteTimes[i], (notePositions[i] - newPositions[i]) / 100, tp_increment)
+                displace(noteTimes[i], (notePositions[i] - newPositions[i]) / 100, tp_increment, sorted_svs)
             end
         end
     end
@@ -117,21 +119,54 @@ function vibe(start, stop, amplitude, increment, stopamplitude, tp_increment, on
     end
     
     --restore displacement to 0 at end
-    time = stop - increment
     target = 0
-    table.insert(svs, sv(time - tp_increment, (target - displacement) / tp_increment))
-    table.insert(svs, sv(time, 0))
+    table.insert(svs, sv(stop - tp_increment, (target - displacement) / tp_increment))
+    table.insert(svs, sv(stop, 0))
     
     mergeSVs(svs)
 end
 
-function displace(time, displacement, increment)
+--assumes SVs are added to map in order
+--[[function correctSVIndexsAtEnd(svs, origin)
+    --identify correct starting index for svs at end of list
+    local dest = getScrollVelocityIndexAt(svs[i].StartTime, svs) + 1
+    
+    local n = #svs - origin + 1
+    local original_size = #svs
+    local temp = {}
+    
+    local j = n
+    for i = origin, #svs do
+        temp[j] = table.remove(svs)
+        j = j - 1
+    end
+    
+    for i = #svs, dest, -1 do
+        svs[i + n] = svs[i]
+    end
+    
+    for i, v in pairs(temp) do
+        svs[dest - 1 + i] = v
+    end
+end
+
+function reverse(A, start, stop)
+    local temp
+    local n = math.floor((stop - start + 1) / 2) - 1
+    for i = 0, n do
+        temp = A[start + i]
+        A[start + i] = A[stop - i]
+        A[stop - i] = temp
+    end
+end]]--
+
+function displace(time, displacement, increment, mapsvs)
     local svs = {}
     table.insert(svs, sv(time - increment, displacement / increment))
     table.insert(svs, sv(time, -1 * displacement / increment))
     table.insert(svs, sv(time + increment, 0))
     
-    mergeSVs(svs)
+    mergeSVs(svs, mapsvs)
 end
 
 function resetCache()
@@ -235,11 +270,13 @@ function performQueue()
     if #action_queue > 0 then actions.PerformBatch(action_queue) end
 end
 
-function mergeSVs(svs)
+function mergeSVs(svs, mapsvs)
+    local mapsvs = mapsvs or map.ScrollVelocities
+    
     --for each sv given, increase map sv if no sv at that time
     for _, sv in pairs(svs) do
         --assumes initial scroll velocity is 1
-        local mapsv = map.GetScrollVelocityAt(sv.StartTime) or utils.CreateScrollVelocity(-1e304, 1)
+        local mapsv = getScrollVelocityAt(sv.StartTime) or utils.CreateScrollVelocity(-1e304, 1)
         if mapsv.StartTime ~= sv.StartTime then
             table.insert(add_sv_queue, utils.CreateScrollVelocity(sv.StartTime, mapsv.Multiplier + sv.Multiplier))
         end
@@ -260,7 +297,7 @@ function mergeSVs(svs)
     local i = 1 --for keeping track of the relevant given sv
     
     --for each map sv within [start, stop), change according to relevant given sv
-    for _, mapsv in pairs(map.ScrollVelocities) do
+    for _, mapsv in pairs(mapsvs) do
         if start <= mapsv.StartTime and mapsv.StartTime < stop then
             --make sure current map sv is between relevant given sv and next given sv
             while mapsv.StartTime >= svs[i+1].StartTime do
@@ -274,4 +311,20 @@ function mergeSVs(svs)
             table.insert(add_sv_queue, utils.CreateScrollVelocity(mapsv.StartTime, mapsv.Multiplier + svs[i].Multiplier))
         end
     end
+end
+
+function getScrollVelocityAt(time, svs)
+    --by default will use the map's svs
+    if not svs or svs == map.ScrollVelocities then return map.GetScrollVelocityAt(time) end
+    
+    --when custom svs are given
+    if #svs == 0 then return nil end
+    if time < svs[1].StartTime then return nil end
+    
+    local i = 1
+    while time >= svs[i+1].StartTime do
+        i = i + 1
+    end
+    
+    return svs[i]
 end
